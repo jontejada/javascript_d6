f0435712f68ce90f05606e07cf416db27baccaf1
Jon Tejada <tejjon@gmail.com>
2015-10-27 09:00:03 -0700

    added all

---
+++ bottles.js
@@ -0,0 +1,10 @@
+
+for (i=99; i>=0; i--) {
+  if (i>1) {
+    console.log(i+" bottles of beer on the wall, "+i+" bottles of beer.\nTake one down and pass it around, "+(i-1)+" bottles of beer on the wall.");
+  } else if (i===1) {
+    console.log(i+" bottle of beer on the wall, "+i+" bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall");
+  } else {
+    console.log("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall");
+  }
+} 
